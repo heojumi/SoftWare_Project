848f197e2cf16e23e5e4b498e2b65de9c77a2fe1
heojumi <hjm4630573@gmail.com>
2021-05-13 22:04:33 +0900

    google map 구현 제발 되게 해주세요

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,17 +5,54 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.app.Application;
+import android.location.Location;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, Application.ActivityLifecycleCallbacks{
+    private GoogleMap mMap;
+    private Marker currentMarker=null;
+
     public static final int REQUEST_CODE_MENU=101;
+    private static final String TAG="googlemap_example";
+    private static final int GPS_ENABLE_REQUEST_CODE=2001;
+    private static final int UPDATE_INTERVAL_MS=1000;
+    private static final int FASTEST_UPDATE_INTERVAL_MS=500;
+
+    private static final int PERMISSION_REQUEST_CODE=100;
+    boolean needRequest = false;
+
+    String[]REQUIRED_PERMISSIONS={Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+
+    Location mCurrentLocation;
+    LatLng currentPosition;
+    private Location location;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest locationRequest;
+    private View view;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         Button button2=findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener(){
@@ -26,6 +63,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public void onMapReady(final GoogleMap googleMap){
+        mMap=googleMap;
+
+        LatLng SEOUL=new LatLng(37.56,126.97);
+
+        MarkerOptions markerOptions=new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        mMap.addMarker(markerOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL,10));
+
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
@@ -39,6 +92,41 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"name: "+name,Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    @Override
+    public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onActivityStarted(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(@NonNull Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(@NonNull Activity activity) {
+
     }
 }
 
