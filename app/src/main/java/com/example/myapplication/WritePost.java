3bcad3698966f29f261f8660f2be784810fccbf9
somsom13 <somin1113@naver.com>
2021-05-17 21:57:10 +0900

    main xml 에러수정

--- app/src/main/java/com/example/myapplication/WritePost.java
+++ app/src/main/java/com/example/myapplication/WritePost.java
@@ -1,19 +1,33 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WritePost extends AppCompatActivity {
 
     private final int GET_IMG_FROM_GALLERY=200;
+    private final int LOCATION_REQUEST=100;
     private ImageView imageview;
+    private EditText editText;
+
+    TextView locationTextView;
+    LocationManager manager;
+    GPSListener gpsListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +42,22 @@ public class WritePost extends AppCompatActivity {
                 startActivityForResult(intent,GET_IMG_FROM_GALLERY);
             }
         });
+    
+        
+        //본문 (content) 내용 string으로 바꿔오기
+        editText=(EditText) findViewById(R.id.content);
+        String mainText=editText.getText().toString();
+
+        //버튼 누르면 현재 위치
+        Button locationbutton=findViewById(R.id.locationbutton);
+        manager=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        //gpsListener=new GPSListener();
+        locationbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
     }
 
     @Override
@@ -38,9 +68,17 @@ public class WritePost extends AppCompatActivity {
             Uri selectedImageUri=data.getData();
             //url db에 저장
             imageview.setImageURI(selectedImageUri);
-
-            
         }
     }
 
+}
+
+abstract class GPSListener implements LocationListener{
+    @Override
+    public void onLocationChanged(Location location){
+        double latitude=location.getLatitude();
+        double longtitude=location.getLongitude();
+        String locMsg="위도: "+latitude+" 경도: "+longtitude;
+      //locationTextView.setText(locMsg);
+    }
 }
