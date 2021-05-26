e9dba8b6fed47a778d21f952a6b101a97d087d4b
chaewan <wani9909@naver.com>
2021-05-26 09:28:05 +0900

    item.xml 추가

--- app/src/main/java/com/example/myapplication/PostActivity.java
+++ app/src/main/java/com/example/myapplication/PostActivity.java
@@ -1,33 +1,26 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class PostActivity extends AppCompatActivity {
+public class PostActivity extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-
-
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_post, container, false);
+        return rootView;
     }
+
 
 }
---
+++ app/src/main/java/com/example/myapplication/onTabItemSelectedListener.java
@@ -0,0 +1,5 @@
+package com.example.myapplication;
+
+public interface onTabItemSelectedListener {
+    public void onTabSelected(int position);
+}
